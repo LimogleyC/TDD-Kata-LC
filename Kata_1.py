c677a852b01368f238b655a9582b9529d8cb5e03
Liam Collins <collinsl25@mymacewan.ca>
2023-01-31 11:53:01 -0700

    Refractor fizzbuzz
    adds to a single string allowing for "fizz", "buzz", & "fizzbuzz"

--- Kata_1.py
+++ Kata_1.py
@@ -1,8 +1,13 @@
 def fizzbuzz(num):
-    rem3 = num % 3
-    if rem3 == 0:
-        return "fizz"
-if __name__ == "__main__":
-
-    x = fizzbuzz(9)
-    print(x)
+    fb = ""             # str to be concatenated
+    
+    # remainders to check division
+    rem3 = num % 3 
+    rem5 = num % 5      
+    
+    if rem3 == 0:       # If divisible by 3 add "fizz"
+        fb += "fizz"
+    if rem5 == 0:       # If divisible by 5 add "buzz"
+        fb += "buzz"
+    
+    return fb           # Return str
--- Kata_1_test.py
+++ Kata_1_test.py
@@ -4,8 +4,21 @@ import Kata_1 as k1
 class Kata_1_test(unittest.TestCase):
     
     def test_fizz(self):
-        # t1 = k1.fizzbuzz(9)
+        self.assertEqual(k1.fizzbuzz(3),"fizz")
         self.assertEqual(k1.fizzbuzz(9),"fizz")
+        self.assertNotEqual(k1.fizzbuzz(0),"fizz")
+        self.assertNotEqual(k1.fizzbuzz(5),"fizz")
+
+    def test_buzz(self):
+        self.assertEqual(k1.fizzbuzz(5),"buzz")
+        self.assertNotEqual(k1.fizzbuzz(0),"buzz")
+        self.assertNotEqual(k1.fizzbuzz(3),"buzz")
+
+    def test_fizzbuzz(self):
+        self.assertEqual(k1.fizzbuzz(15), "fizzbuzz")
+        self.assertNotEqual(k1.fizzbuzz(3),"fizzbuzz")
+        self.assertNotEqual(k1.fizzbuzz(5),"fizzbuzz")
+
 
 if __name__ == "__main__":
     unittest.main()
