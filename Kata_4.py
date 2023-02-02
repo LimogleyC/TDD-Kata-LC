941e8f2ef803135f96e9e863dc2602640d23a296
Liam Collins <collinsl25@mymacewan.ca>
2023-02-02 10:35:03 -0700

    Completed Kata 4
    - Search is now case insensitive
    - Added * input to return all
    - Now returns list of cities
    - Added tests for all expected cases

--- Kata_4.py
+++ Kata_4.py
@@ -6,9 +6,21 @@ def too_short(inp):
     if len(inp) < 2:
         return True
 
+def parse(inp, result):
+    for name in cities:
+        if inp.lower() in name.lower():
+            result.append(name)
+    return(result)
+
 def search(inp):
+    if inp == "*":
+        return(cities)
     if too_short(inp):
         return None
-    for name in cities:
-        if inp in name:
-            return name
+    result = []
+    return(parse(inp, result))
+
+# inp = input("ASDFKAJSDFHASLFD: ")
+# while inp != "0":
+#     print(search(inp))
+#     inp = input("\nBRA bre bring bro: ")
--- Kata_4_test.py
+++ Kata_4_test.py
@@ -5,11 +5,26 @@ class Kata_4_test(unittest.TestCase):
     def test_shouldReturnNone_whenInputLT2(self):
         self.assertTrue(k4.too_short("B"))
     
+    def test_shouldReturnValenciaVancouver_whenInputVa(self):
+        self.assertTrue(k4.search("Va"), ["Valencia", "Vancouver"])
+
     def test_shouldPrintParis_whenInputaris(self):
-        self.assertEqual(k4.search("aris"), "Paris")
+        self.assertEqual(k4.search("aris"), ["Paris"])
 
     def test_shouldPrintParis_whenInputAris(self):
-        self.assertEqual(k4.search("Aris"), "Paris")
+        self.assertEqual(k4.search("ARIS"), ["Paris"])
+
+    def test_shouldPrintParis_whenInputAris(self):
+        self.assertEqual(k4.search("ArIs"), ["Paris"])
+
+    def test_shouldPrintSkopjeBangkokHongKong_whenInputko(self):
+        self.assertEqual(k4.search("ko"), ["Skopje", "Bangkok", "Hong Kong"])
+
+    def test_shouldPrintAll_whenInputAsteriks(self):
+        self.assertEqual(k4.search("*"), ["Paris", "Budapest", "Skopje", 
+            "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", 
+            "Sydney", "New York City", "London", "Bangkok", "Hong Kong", 
+            "Dubai", "Rome", "Istanbul"])
 
 if __name__ == "__main__":
     unittest.main()
